e348e1670b723c729107253048b9958017636614
Dynastiy <emmanuel00michael@gmail.com>
2022-03-31 11:11:49 +0100

    fixed pagination

--- src/views/pages/index.vue
+++ src/views/pages/index.vue
@@ -2,15 +2,12 @@
     <div>
         
             <router-view :key="$route.fullPath" />
-            <pagination/>
     </div>
 </template>
 
 <script>
-import pagination from '@/components/pagination'
+
 export default {
-    components:{
-        pagination 
-    }
+   
 }
 </script>
